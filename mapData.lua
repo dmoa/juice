ba245e5d07cacf801c62b164d805232180fce9bc
Stan <spammyboy@protonmail.com>
2019-10-22 20:17:51 +0100

    more maps & offsets

--- mapData.lua
+++ mapData.lua
@@ -1,16 +1,26 @@
 return {
-    -- offset only mentioned if required, i.e. tile doesn't fit
+    -- offset / width / height only mentioned if required, i.e. tile doesn't fit
     -- proportions
     xOffsets = {
-        _49 = 5, _50 = 5, _57 = 5, _58 = 5
+        -- signposts
+        _49 = 5, _50 = 5, _57 = 5, _58 = 5,
     },
     yOffsets = {
-        _49 = 10, _50 = 10, _57 = 10, _58 = 10
+        -- signposts
+        _49 = 10, _50 = 10, _57 = 10, _58 = 10,
+        -- bottom wall
+        _82 = 11
     },
     widths = {
-        _49 = 5, _50 = 5, _57 = 5, _58 = 5
+        -- signposts
+        _49 = 5, _50 = 5, _57 = 5, _58 = 5,
+        -- left side wall
+        _73 = 5
     },
     heights = {
-        _49 = 2, _50 = 2, _57 = 2, _58 = 2
+        -- signposts
+        _49 = 2, _50 = 2, _57 = 2, _58 = 2,
+        -- bottom wall
+        _82 = 5
     }
 }
