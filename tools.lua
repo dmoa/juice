285a71fad12651788dcac1c688e33966f743d333
dmoa <spammyboy@protonmail.com>
2019-10-25 22:08:25 +0100

    collision functions and better controller handling

--- tools.lua
+++ tools.lua
@@ -3,6 +3,19 @@ function AABB(x1, y1, w1, h1, x2, y2, w2, h2)
         and y1 + h1 > y2 and y1 < y2 + h2
 end
 
+function AABBTouching(x1, y1, w1, h1, x2, y2, w2, h2)
+    return x1 + w1 >= x2 and x1 <= x2 + w2 
+        and y1 + h1 >= y2 and y1 <= y2 + h2
+end
+
+function AABBHorizontal(x1, w1, x2, w2)
+    return x1 + w1 > x2 and x1 < x2 + w2 
+end
+
+function AABBVertical(y1, h1, y2, h2)
+    return y1 + h1 > y2 and y1 < y2 + h2 
+end
+
 pixelatedShader = love.graphics.newShader([[
 
     extern vec2 size;
@@ -15,19 +28,19 @@ pixelatedShader = love.graphics.newShader([[
 ]])
 
 joystick = love.joystick.getJoysticks()[1]
-joystick:setVibration(1, 1, 0.5)
+-- joystick:setVibration(1, 1, 0.5)
 
 keyIsDown = {
     right = function()
-        return (love.keyboard.isDown("right") or love.keyboard.isDown("d") or joystick:getAxis(1) > 0.5)
+        return (love.keyboard.isDown("right") or love.keyboard.isDown("d") or (joystick and joystick:getAxis(1) > 0.5))
     end,
     left = function()
-        return (love.keyboard.isDown("left") or love.keyboard.isDown("a") or joystick:getAxis(1) < -0.5)
+        return (love.keyboard.isDown("left") or love.keyboard.isDown("a") or (joystick and joystick:getAxis(1) < -0.5))
     end,
     up = function()
-        return (love.keyboard.isDown("up") or love.keyboard.isDown("w") or joystick:getAxis(2) < -0.5)
+        return (love.keyboard.isDown("up") or love.keyboard.isDown("w") or (joystick and joystick:getAxis(2) < -0.5))
     end,
     down = function()
-        return (love.keyboard.isDown("down") or love.keyboard.isDown("s") or joystick:getAxis(2) > 0.5)
+        return (love.keyboard.isDown("down") or love.keyboard.isDown("s") or (joystick and joystick:getAxis(2) > 0.5))
     end
 }
