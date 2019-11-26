cbb4d47f6b94b9da48f755442a491306f380cfe8
dmoa <spammyboy@protonmail.com>
2019-11-26 16:41:07 +0000

    lg"

--- 3d.lua
+++ 3d.lua
@@ -1,10 +1,12 @@
 local ss3dScene = {}
 
-local scene = ss3dEngine.newScene(love.graphics.getWidth(), love.graphics.getHeight())
+local scene = ss3dEngine.newScene(lg.getWidth(), lg.getHeight())
 local Timer = 0
 
-scene.camera.pos.x = 5
-scene.camera.pos.z = 5
+local canvas = lg.newCanvas()
+
+scene.camera.pos.x = 0
+scene.camera.pos.z = 8
 
 -- local pyramidVerts = {}
 -- pyramidVerts[#pyramidVerts+1] = {-1,-1,-1}
@@ -25,26 +27,34 @@ scene.camera.pos.z = 5
 
 -- Pyramids = {}
 -- for i=1, 4 do
---     Pyramids[#Pyramids+1] = ss3dEngine.newModel(pyramidVerts, love.graphics.newImage("imgs/3Dtexture.png"))
+--     Pyramids[#Pyramids+1] = ss3dEngine.newModel(pyramidVerts, lg.newImage("imgs/3Dtexture.png"))
 --     Pyramids[i].wireframe = true
 --     scene:addModel(Pyramids[i])
 -- end
 
-local square = ss3dEngine.newModel(ss3dEngine.loadObj("3dObjects/square.obj"), love.graphics.newImage("imgs/3Dtexture.png"))
+local square = ss3dEngine.newModel(ss3dEngine.loadObj("3dObjects/square.obj"), lg.newImage("imgs/3Dtexture.png"))
 scene:addModel(square)
 
 function ss3dScene:draw()
-    local canvas, x, y, width, height, rotation, scaleX, scaleY, offsetX, offsetY = scene:getRender()
-    if shaderOn then lg.setShader(crtShader) end
-    lg.draw(canvas, x, y, width, height, rotation, scaleX, scaleY, offsetX, offsetY)
+    local engineCanvas, x, y, width, height, rotation, scaleX, scaleY, offsetX, offsetY = scene:getRender()
+
+    --lg.setShader(pixelatedShader)
+    lg.setCanvas(canvas)
+    lg.clear()
+    lg.draw(engineCanvas, x, y, width, height, rotation, scaleX, scaleY, offsetX, offsetY)
+    lg.setCanvas()
+
+    --if shaderOn then lg.setShader(crtShader) end
+    lg.draw(canvas)
 end
 
 function ss3dScene:update(dt)
-    -- Timer = Timer + dt/4
+    Timer = Timer + dt * 3
 
     -- for i=1, #Pyramids do
     --     Pyramids[i]:setTransform({}, {Timer, cpml.vec3.unit_y, Timer, cpml.vec3.unit_z, Timer, cpml.vec3.unit_x})
     -- end
+    square:setTransform({}, {Timer, cpml.vec3.unit_y, 2, cpml.vec3.unit_z, 0, cpml.vec3.unit_x})
 end
 
 return ss3dScene
