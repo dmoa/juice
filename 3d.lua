ed5d5703c242ef3c4fcc587450bbd3bddd9b6851
dmoa <spammyboy@protonmail.com>
2019-11-26 14:33:03 +0000

    see previous commit
    forgot to add this file ree

--- 3d.lua
+++ 3d.lua
@@ -3,7 +3,7 @@ local ss3dScene = {}
 local scene = ss3dEngine.newScene(love.graphics.getWidth(), love.graphics.getHeight())
 local Timer = 0
 
-scene.camera.pos.x = 0
+scene.camera.pos.x = 5
 scene.camera.pos.z = 5
 
 local pyramidVerts = {}
@@ -25,13 +25,18 @@ pyramidVerts[#pyramidVerts+1] = {1,-1,1}
 
 Pyramids = {}
 for i=1, 4 do
-    Pyramids[#Pyramids+1] = ss3dEngine.newModel(pyramidVerts)
+    Pyramids[#Pyramids+1] = ss3dEngine.newModel(pyramidVerts, love.graphics.newImage("imgs/3Dtexture.png"))
     Pyramids[i].wireframe = true
     scene:addModel(Pyramids[i])
 end
 
+AlakazamModel = ss3dEngine.newModel(ss3dEngine.loadObj("alakazam.obj"), love.graphics.newImage("imgs/3Dtexture.png"))
+--scene:addModel(AlakazamModel)
+
 function ss3dScene:draw()
-    scene:render()
+    local canvas, x, y, width, height, rotation, scaleX, scaleY, offsetX, offsetY = scene:getRender()
+    if shaderOn then lg.setShader(crtShader) end
+    lg.draw(canvas, x, y, width, height, rotation, scaleX, scaleY, offsetX, offsetY)
 end
 
 function ss3dScene:update(dt)
