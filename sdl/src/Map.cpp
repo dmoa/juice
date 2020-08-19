0d47c30094b76f6c90a29c8313a5f00bad2eb9b0
Stan O <spammyboy@protonmail.com>
2020-08-19 14:22:36 +0100

    fixed corner water tiles

--- sdl/src/Map.cpp
+++ sdl/src/Map.cpp
@@ -26,29 +26,45 @@ void Map::CreateMapTexture() {
     }
 
     iter_quad.y = 16; // switching to row of water quads
-
     iter_quad.x = 48;
 
     iter_pos.x = iter_pos.y = 0;
     SDL_RenderCopy(global_window_data.rdr, texture, & iter_quad, & iter_pos);
     iter_pos.x = (tiles_wide - 1) * tile_length;
     SDL_RenderCopy(global_window_data.rdr, texture, & iter_quad, & iter_pos);
-    iter_pos.x = iter_pos.y = 0, (tiles_high - 1) * tile_length;
+    iter_pos.x = 0;
+    iter_pos.y = (tiles_high - 1) * tile_length;
     SDL_RenderCopy(global_window_data.rdr, texture, & iter_quad, & iter_pos);
     iter_pos.x = (tiles_wide - 1) * tile_length;
     SDL_RenderCopy(global_window_data.rdr, texture, & iter_quad, & iter_pos);
 
+    iter_pos.y = 0;
     iter_quad.x = 80;
     for (int i = 1; i < tiles_wide - 1; i++) {
         iter_pos.x = i * tile_length;
         SDL_RenderCopy(global_window_data.rdr, texture, & iter_quad, & iter_pos);
     }
 
+
     iter_quad.x = 32;
+
     iter_pos.x = 0;
     for (int i = 1; i < tiles_high - 1; i++) {
         iter_pos.y = i * tile_length;
         SDL_RenderCopyEx(global_window_data.rdr, texture, & iter_quad, & iter_pos, NULL, NULL, SDL_FLIP_HORIZONTAL);
+    }
+
+    iter_pos.x = (tiles_wide - 1) * tile_length;
+    for (int i = 1; i < tiles_high - 1; i++) {
+        iter_pos.y = i * tile_length;
+        SDL_RenderCopy(global_window_data.rdr, texture, & iter_quad, & iter_pos);
+    }
+
+    iter_quad.x = 80;
+    iter_pos.y = (tiles_high - 1) * tile_length;
+    for (int i = 1; i < tiles_wide - 1; i++) {
+        iter_pos.x = i * tile_length;
+        SDL_RenderCopyEx(global_window_data.rdr, texture, & iter_quad, & iter_pos, NULL, NULL, SDL_FLIP_VERTICAL);
     }
 
 
@@ -73,8 +89,6 @@ CollisionBoxes* Map::GetCollisionBoxes() {
 
 void Map::Draw() {
     map_rendering_quad = {0, 0, global_window_data.w / global_window_data.scale, global_window_data.h / global_window_data.scale};
-    // SDL_Log("%i", map_rendering_quad.w);
-    // SDL_Log();
     SDL_RenderCopy(global_window_data.rdr, saved_drawn_data, & map_rendering_quad, NULL);
 }
 
