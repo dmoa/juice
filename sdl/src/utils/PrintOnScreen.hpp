8e969d22fff635838e34b5487fb1406b210d3f0e
Stan O <spammyboy@protonmail.com>
2020-09-01 15:01:44 +0100

    print to screen

---
+++ sdl/src/utils/PrintOnScreen.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+struct PrintOnScreen {
+    SDL_Rect rect = {5, 0, 50, 50};
+    SDL_Texture* fps_texture = NULL;
+    void Draw(std::string text, int x, int y) {
+        rect.x = x; rect.y = y;
+        fps_texture = Text::CreateTexture(text);
+        SDL_QueryTexture(fps_texture, NULL, NULL, & rect.w, & rect.h);
+        SDL_RenderCopy(global_window_data.rdr, fps_texture, NULL, & rect);
+        SDL_DestroyTexture(fps_texture);
+    }
+};
